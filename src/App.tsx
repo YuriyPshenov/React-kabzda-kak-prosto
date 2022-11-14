a7506cb2334f6843ef55b3118e93e22251200f0b
YuriyPshenov <yuriypshenov@gmail.com>
2022-11-14 13:23:41 +0300

    React-kabzda-kak-prosto-01

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    // что-то полезное
+
+    // обязана вернуть JSX
+    debugger
+    return (
+        <div>
+            This is APP component
+            <Rating />
+            <Accordion />
+        </div>
+    );
+}
+
+function Rating() {
+    debugger
+    return (
+        <div>
+            <Star />
+            <Star />
+            <Star />
+            <Star />
+            <Star />
+        </div>
+    )
+}
+
+function Accordion() {
+    debugger
+    return <div>
+        <h3>Menu</h3>
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
     </div>
-  );
+}
+
+function Star() {
+    return <div>Star</div>
 }
 
 export default App;
