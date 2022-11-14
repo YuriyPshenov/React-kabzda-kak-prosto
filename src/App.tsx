899865c3806cefd6a31b8ef25bdaa0faae6f1b14
YuriyPshenov <yuriypshenov@gmail.com>
2022-11-14 13:46:09 +0300

    React-kabzda-kak-prosto-02

--- src/App.tsx
+++ src/App.tsx
@@ -2,47 +2,63 @@ import React from 'react';
 import './App.css';
 
 function App() {
-
+    console.log('app rendering')
     // что-то полезное
 
     // обязана вернуть JSX
-    debugger
     return (
         <div>
-            This is APP component
+            <AppTitle />
+            <Rating />
+            <Accordion />
             <Rating />
             <Accordion />
         </div>
+
     );
 }
 
+function AppTitle() {
+    console.log('apptitle rendering')
+    return <>This is App Component</>
+}
+
 function Rating() {
-    debugger
+    console.log('rating rendering')
     return (
-        <div>
+        <>
             <Star />
             <Star />
             <Star />
             <Star />
             <Star />
-        </div>
+        </>
     )
 }
 
 function Accordion() {
-    debugger
+    console.log('accordion rendering')
     return <div>
-        <h3>Menu</h3>
-        <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-        </ul>
+        <AccordionTitle />
+        <AccordionBody />
     </div>
 }
 
 function Star() {
+    console.log('star rendering')
     return <div>Star</div>
+}
+
+function AccordionTitle() {
+    return <h3>Menu</h3>
+}
+
+function AccordionBody() {
+    return <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+    </ul>
 }
 
 export default App;
