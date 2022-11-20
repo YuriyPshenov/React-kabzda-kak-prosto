ef1920f1663bbfad7db42dd3df83abe00e5490a7
YuriyPshenov <yuriypshenov@gmail.com>
2022-11-20 21:02:34 +0300

    React-kabzda-kak-prosto-13

--- src/stories/Input.stories.tsx
+++ src/stories/Input.stories.tsx
@@ -1,4 +1,5 @@
 import React, {ChangeEvent, useRef, useState, } from 'react';
+import {action} from "@storybook/addon-actions";
 
 
 export default {
@@ -26,6 +27,40 @@ export const GetValueOfUncontrolledInputByButtonPress = () => {
 }
 
   return <><input ref={inputRef} id={'inputId'} /><button onClick={onClickHandler}>save</button> - actual value - {value} </>
+}
+
+export const ControlledInput = () => {
+    const [parentValue, setParentValue] = useState('')
+    const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        setParentValue(event.currentTarget.value)
+    }
+
+    return <input value={parentValue} onChange={onChangeHandler}/>
+}
+
+export const ControlledCheckbox = () => {
+    const [parentValue, setParentValue] = useState(true)
+    const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        setParentValue(event.currentTarget.checked)
+    }
+
+    return <input type={'checkbox'} checked={parentValue} onChange={onChangeHandler}/>
+}
+
+export const ControlledSelect = () => {
+    const [parentValue, setParentValue] = useState<string | undefined>('2')
+    const onChangeHandler = (event: ChangeEvent<HTMLSelectElement>) => {
+        setParentValue(event.currentTarget.value)
+    }
+
+    return (
+        <select value={parentValue} onChange={onChangeHandler}>
+            <option>none</option>
+            <option value={'1'}>Minsk</option>
+            <option value={'2'}>Moscow</option>
+            <option value={'3'}>Kiev</option>
+        </select>
+    )
 }
 
 export const ControlledInputWithFixedValue = () => <input value={'it-incubator.by'}/>
